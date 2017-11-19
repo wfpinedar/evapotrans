bc099e610464aad659c918a860feed833283a091
Kanocampillo <kanocampillo@gmail.com>
2017-11-19 18:24:40 -0500

    new har

--- main_pos.py
+++ main_pos.py
@@ -5,11 +5,13 @@ from evapot.load_variable import load_variable
 from evapot.add_geoColum import add_geoColum
 from evapot.export_2_excel import make_excel, get_table, get_var
 from evapot.load_formulas import add_formulas
+from evapot.load_rad_har import load_rad_har
 
 def main():
     DB="evot"
 ##    build_db()
-    load_station(r"E:\DOCUMENTOS\TRABAJOS INDEPENDIENTES\PROYECTO_EVAPOTRANS\datos_demo\Estaciones\estaciones.csv",DB,"estacion_fix")
+##    load_station(r"E:\DOCUMENTOS\TRABAJOS INDEPENDIENTES\PROYECTO_EVAPOTRANS\datos_demo\Estaciones\estaciones.csv",DB,"estacion_fix")
+    load_rad_har(r"E:\DOCUMENTOS\TRABAJOS INDEPENDIENTES\PROYECTO_EVAPOTRANS\datos_demo\tabla_radiacion_har.csv",DB,"rad_extra_har")
 ##    add_geoColum(DB,"estacion")
 ##    load_variable(r"C:\datos_demo\Variables\AJUSTADAS\TEMPERATURA_MEDIA.csv",DB,"variable")
 ##    load_variable(r"C:\datos_demo\Variables\AJUSTADAS\TEMPERATURA_MINIMA.csv",DB,"variable")
